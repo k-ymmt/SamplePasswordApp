5a47682d54db35e929db57c0fdde68e3e5b27812
k-ymmt <account@kymmt.net>
2020-11-17 14:46:54 +0900

    project init

---
+++ SamplePasswordApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SamplePasswordApp
+//
+//  Created by Kazuki Yamamoto on 2020/11/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var password1: UITextField!
+    @IBOutlet weak var password2: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
